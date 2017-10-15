c9296887e8c091613ad1cb95631f8669d13c0475
pestelle <nicolas.pestelle@hotmail.fr>
2017-10-15 17:21:44 +0200

    pris en comtpe du texte, plus que les maj a faire et casser le code

--- TP1/TP1.cpp
+++ TP1/TP1.cpp
@@ -5,93 +5,84 @@
 using namespace std;
 
 string chiffrement (string texte, string cle){
- cout<<"je chiffre"<< endl;
 
+  int indiceCle = 0;
 
- int indiceCle = 0;
-
- string crypte;
+  string crypte;
  
   for (int i = 0; i < texte.length();i++){
-   if ((int)texte[i]>= 62 && (int)texte[i]<=90 || (int)texte[i]>=97 && (int)texte[i]<=122){
-     crypte += ((int)texte[i]-'a' + cle[indiceCle]-'a')%26  + 'a';
-	 cout << crypte << endl;
-   }
+    if ((int)texte[i]>= 62 && (int)texte[i]<=90 || (int)texte[i]>=97 && (int)texte[i]<=122){
+      crypte += ((int)texte[i]-'a' + cle[indiceCle]-'a')%26  + 'a';
+    }else{
+      crypte += texte[i];
+    }
 
-        indiceCle ++;
+    indiceCle ++;
      
-     if(indiceCle == cle.length()){
-       indiceCle = 0;
-       }
+    if(indiceCle == cle.length()){
+      indiceCle = 0;
+    }
      
- }
+  }
   return crypte;
      
-   }
+}
 
 
-void dechiffrement(string texte, string cle){
-  cout << "je dechiffre"<< endl; 
-  string crypte;
+string dechiffrement(string texte, string cle){
+ string decrypte;
  int indiceCle = 0;
   for (int i = 0; i < texte.length();i++){
    if ((int)texte[i]>= 62 && (int)texte[i]<=90 || (int)texte[i]>=97 && (int)texte[i]<=122){
-     crypte += (((int)texte[i]-'a') - (cle[indiceCle]-'a')+26)%26  + 'a';
-	 cout << crypte << endl;
-   }
+     decrypte += (((int)texte[i]-'a') - (cle[indiceCle]-'a')+26)%26  + 'a';
+   }else{
+      decrypte += texte[i];
+    }
 
         indiceCle ++;
      
      if(indiceCle == cle.length()){
        indiceCle = 0;
-       }
-     
+       }     
  }
+  return decrypte;
 }
 
 int main(int argc,char** argv){
 
   string texte = argv[2];
   string cle = argv[3];
-
-
+  string result;
   
-  ifstream fichier(argv[2], ios::in);  // on ouvre en lecture
- 
-  if(fichier)  // si l'ouverture a fonctionné
-    {
-      string ligne;
-
-      while(getline(fichier, ligne))  // tant que l'on peut mettre la ligne dans "contenu"
-
-        {
-
-	  cout << ligne << endl;  // on l'affiche
-
-        }
-    }
-  else
-    cerr << "Impossible d'ouvrir le fichier !" << endl;
-
-
-  
-
-  //  comparer chaine de carac en c++
   if(argc<4){
     cout<<"Pas assez d'arguments, utiliser ./TP1.out <option> <texte> <cle>"<<endl;
   }else{
-    if(argv[1] == string("-c"))
-      //on chiffre
-      chiffrement((char*)argv[2], (char*)argv[3]);
-    else if(argv[1] == string("-d"))
-      //on dechiffre
-      cout<<"je dechiffre"<< endl;
-    else cout << "argument invalide"<< endl;
-  }
+    if(argv[1] == string("-c")){
+      
+      std::ifstream ifs(argv[2]);
+      std::string line;
+      std::ofstream ofs("texteChiffre.txt");
+      
+      while (std::getline(ifs, line)) { 
+	 ofs << chiffrement(line, (char*)argv[3]);
+      }
+      
+    }else if(argv[1] == string("-d")){
+      
+      std::ifstream ifs(argv[2]);
+      std::string line;
+      std::ofstream ofs("texteDechiffre.txt");
 
-  /*string crypte =  chiffrement(texte,cle);
-    dechiffrement(crypte, cle);*/
-  
+      while (std::getline(ifs, line)) { 
+	ofs << dechiffrement(line, (char*)argv[3]);
+      }
+      
+    }else{
+      
+      cout << "argument invalide"<< endl;
+      
+    }
+  }
 
   return 0;
 
